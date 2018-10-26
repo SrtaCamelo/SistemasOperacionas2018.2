8821def37a5376e1c4b68442e99d9ea5ddc34cd9
unknown <raissa_camelo@hotmail.com>
2018-10-26 20:37:47 -0300

    Sistemas Operacionais

--- SO_Processos/processo_Buffer.py
+++ SO_Processos/processo_Buffer.py
@@ -1,2 +1,37 @@
+#Sistemas Operacionas 2018.2
+#Raissa Camelo
+#Atividade: Processos - Buffer
+
+#----------------Def Structs---------------
 buffer = []
-buffer_size = 10
+buffer_size = 5
+buffer_index = 0
+
+def appendBuffer(item,buffer_index):
+    if buffer_index < buffer_size:
+        buffer.append(item)
+        buffer_index += 1
+        print("Um item foi adicionado ao Buffer")
+    else:
+        print("Não pode adicionar ao Buffer, está lotado")
+    return buffer_index
+def removeBuffer(buffer_index):
+    if buffer_index > 0:
+        buffer.pop()
+        buffer_index -= 1
+        print("Item removido do buffer")
+    else:
+        print("Não pode remover, buffer vazio")
+    return buffer_index
+
+#-------------------Main-----------------------
+#----- A: add no buffer
+#----- R: remove do buffer
+while True:
+    print(buffer)
+    char = input("Digite a opção: \n")
+    if char == 'A':
+        item = input("Digite item a ser add:\n")
+        buffer_index = appendBuffer(item,buffer_index)
+    elif char == 'R':
+        buffer_index = removeBuffer(buffer_index)
--- SO_Processos/processo_PaiEFilho.py
+++ SO_Processos/processo_PaiEFilho.py
@@ -5,23 +5,18 @@
 from anytree import Node, RenderTree
 
 #-----------Estuturas-------------
-process_list = []
 
-def create_process(parent_id, parent):
-    process_id = parent_id + 1
+
+def process2():
+    print("Processo filho")
+def process3():
+    print("Processo filho dois")
+def create_process():
+    print("Entrou no processo Pai")
+    process2()
+    process3()
+    print("Saiu do pai")
+
 
 #------------------Main Code------------
-"""
-BOTÕES:
-N : Cria novo processo (Filho)
-V : Muda pra janela do bloco de notas (minimizada)
-"""
-def so():
-    on = True
-    while on:
-        input_stream = input()
-        if input_stream == 'B':
-            create_process(0,0)
-        elif input_stream == 'V':
-#----------Turn On S.O
-so()
+create_process()
